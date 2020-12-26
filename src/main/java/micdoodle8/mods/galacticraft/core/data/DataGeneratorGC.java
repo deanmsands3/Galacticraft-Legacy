06f9e9851ed440d3c79c9332aa17578b4b100404
SteveKunG <mccommander_minecraft@hotmail.com>
2020-12-26 19:42:48 +0700

    Clean up models, various fix for texture location

--- src/main/java/micdoodle8/mods/galacticraft/core/data/DataGeneratorGC.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/data/DataGeneratorGC.java
@@ -2,7 +2,9 @@ package micdoodle8.mods.galacticraft.core.data;
 
 import micdoodle8.mods.galacticraft.core.Constants;
 import micdoodle8.mods.galacticraft.core.GCBlocks;
+import micdoodle8.mods.galacticraft.core.fluid.GCFluids;
 import net.minecraft.block.Block;
+import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.item.Item;
 import net.minecraft.util.IItemProvider;
@@ -50,6 +52,12 @@ public class DataGeneratorGC
         @Override
         protected void registerStatesAndModels()
         {
+            ResourceLocation tinDecor = this.modLoc("block/tin_decoration_block");
+            ResourceLocation machineBase = this.modLoc("block/machine_base");
+            ResourceLocation machineSide = this.modLoc("block/machine_side");
+            ResourceLocation machineInput = this.modLoc("block/machine_input");
+            ResourceLocation machineOutput = this.modLoc("block/machine_output");
+
             this.simpleBlock(GCBlocks.COPPER_ORE);
             this.simpleBlock(GCBlocks.TIN_ORE);
             this.simpleBlock(GCBlocks.ALUMINUM_ORE);
@@ -68,38 +76,98 @@ public class DataGeneratorGC
             this.simpleBlock(GCBlocks.MOON_DUNGEON_BRICKS);
             this.simpleBlock(GCBlocks.EMERGENCY_POST, this.models().getExistingFile(this.modLoc("block/emergency_post")));
             this.simpleBlock(GCBlocks.EMERGENCY_POST_KIT, this.models().getExistingFile(this.modLoc("block/emergency_post_kit")));
+            this.simpleBlock(GCBlocks.FALLEN_METEOR, this.models().getExistingFile(this.modLoc("block/fallen_meteor")));
             this.simpleBlock(GCBlocks.AIR_LOCK_FRAME);
             this.simpleBlock(GCBlocks.TIER_1_TREASURE_CHEST);
+            this.simpleBlock(GCBlocks.TIN_DECORATION_BLOCK_1, this.models().cubeAll("tin_decoration_block_1", tinDecor));
+            this.simpleBlock(GCBlocks.OXYGEN_DETECTOR, this.models().cubeColumn("oxygen_detector", this.modLoc("block/oxygen_detector"), machineBase));
+            this.simpleBlock(GCBlocks.TELEMETRY_UNIT);
+            this.simpleBlock(GCBlocks.BREATHEABLE_AIR, this.models().getExistingFile(this.mcLoc("block/barrier")));
+            this.simpleBlock(GCBlocks.BRIGHT_AIR, this.models().getExistingFile(this.mcLoc("block/barrier")));
+            this.simpleBlock(GCBlocks.BRIGHT_BREATHEABLE_AIR, this.models().getExistingFile(this.mcLoc("block/barrier")));
+            this.simpleBlock(GCBlocks.MOON_BOSS_SPAWNER, this.models().getExistingFile(this.mcLoc("block/barrier")));
 
             ModelFile model = this.models().cubeBottomTop("air_lock_controller", this.modLoc("block/air_lock_controller"), this.modLoc("block/air_lock_frame"), this.modLoc("block/air_lock_frame"));
             this.simpleBlock(GCBlocks.AIR_LOCK_CONTROLLER, model);
 
-            model = this.models().cubeAll("player_detector", this.modLoc("block/tin_decoration"));
+            model = this.models().cube("oxygen_bubble_distributor", machineBase, machineBase, this.modLoc("block/oxygen_bubble_distributor"), this.modLoc("block/oxygen_bubble_distributor"), machineInput, this.modLoc("block/machine_oxygen_input")).texture("particle", this.modLoc("block/oxygen_bubble_distributor"));
+            this.horizontalBlock(GCBlocks.OXYGEN_BUBBLE_DISTRIBUTOR, model);
+
+            model = this.models().cube("oxygen_collector", this.modLoc("block/oxygen_collector"), this.modLoc("block/oxygen_collector"), this.modLoc("block/oxygen_collector"), this.modLoc("block/oxygen_collector"), machineInput, this.modLoc("block/machine_oxygen_output")).texture("particle", this.modLoc("block/oxygen_collector"));
+            this.horizontalBlock(GCBlocks.OXYGEN_COLLECTOR, model);
+
+            model = this.models().cubeBottomTop("tin_decoration_block_2", this.modLoc("block/tin_decoration_block_side"), tinDecor, this.modLoc("block/tin_decoration_block_top"));
+            this.simpleBlock(GCBlocks.TIN_DECORATION_BLOCK_2, model);
+
+            model = this.models().orientable("compressor", machineSide, this.modLoc("block/compressor"), machineBase).texture("particle", this.modLoc("block/compressor"));
+            this.horizontalBlock(GCBlocks.COMPRESSOR, model);
+
+            //model = this.models().cube("electric_compressor", machineBase, machineBase, this.modLoc("block/electric_compressor"), machineSide, machineInput, machineSide).texture("particle", this.modLoc("block/electric_compressor"));
+            //this.horizontalBlock(GCBlocks.ELECTRIC_COMPRESSOR, model);
+
+            model = this.models().cube("coal_generator", machineBase, machineBase, this.modLoc("block/coal_generator"), machineSide, machineOutput, machineSide).texture("particle", this.modLoc("block/coal_generator"));
+            this.horizontalBlock(GCBlocks.COAL_GENERATOR, model);
+
+            model = this.models().cube("chromatic_applicator", this.modLoc("block/chromatic_applicator"), this.modLoc("block/chromatic_applicator"), this.modLoc("block/chromatic_applicator"), this.modLoc("block/chromatic_applicator"), machineSide, machineSide).texture("particle", this.modLoc("block/chromatic_applicator"));
+            this.horizontalBlock(GCBlocks.CHROMATIC_APPLICATOR, model);
+
+            model = this.models().orientableWithBottom("basic_solar_panel", this.modLoc("block/basic_solar_panel_side"), machineOutput, machineBase, this.modLoc("block/basic_solar_panel_top")).texture("particle", this.modLoc("block/basic_solar_panel_side"));
+            this.horizontalBlock(GCBlocks.BASIC_SOLAR_PANEL, model);
+            model = this.models().orientableWithBottom("advanced_solar_panel", this.modLoc("block/advanced_solar_panel_side"), machineOutput, machineBase, this.modLoc("block/advanced_solar_panel_top")).texture("particle", this.modLoc("block/advanced_solar_panel_side"));
+            this.horizontalBlock(GCBlocks.ADVANCED_SOLAR_PANEL, model);
+
+            model = this.models().cube("oxygen_compressor", machineBase, machineBase, this.modLoc("block/oxygen_compressor_back"), this.modLoc("block/oxygen_compressor"), machineInput, this.modLoc("block/machine_oxygen_input")).texture("particle", this.modLoc("block/oxygen_compressor"));
+            this.horizontalBlock(GCBlocks.OXYGEN_COMPRESSOR, model);
+            model = this.models().cube("oxygen_decompressor", machineBase, machineBase, this.modLoc("block/oxygen_compressor_back"), this.modLoc("block/oxygen_decompressor"), machineInput, this.modLoc("block/machine_oxygen_output")).texture("particle", this.modLoc("block/oxygen_decompressor"));
+            this.horizontalBlock(GCBlocks.OXYGEN_DECOMPRESSOR, model);
+
+            model = this.models().cube("oxygen_sealer", machineBase, this.modLoc("block/oxygen_sealer"), machineBase, machineBase, machineInput, this.modLoc("block/machine_oxygen_input")).texture("particle", this.modLoc("block/oxygen_sealer"));
+            this.horizontalBlock(GCBlocks.OXYGEN_SEALER, model);
+
+            model = this.models().cube("refinery", machineBase, machineInput, this.modLoc("block/refinery_front"), this.modLoc("block/refinery_back"), this.modLoc("block/machine_oil_input"), this.modLoc("block/machine_fuel_input")).texture("particle", this.modLoc("block/refinery_front"));
+            this.horizontalBlock(GCBlocks.REFINERY, model);
+
+            model = this.models().cube("cargo_loader", machineBase, machineBase, this.modLoc("block/cargo_loader"), this.modLoc("block/cargo_loader"), machineInput, this.modLoc("block/machine_item_input")).texture("particle", this.modLoc("block/cargo_loader"));
+            this.horizontalBlock(GCBlocks.CARGO_LOADER, model);
+            model = this.models().cube("cargo_unloader", machineBase, machineBase, this.modLoc("block/cargo_unloader"), this.modLoc("block/cargo_unloader"), machineInput, this.modLoc("block/machine_item_output")).texture("particle", this.modLoc("block/cargo_unloader"));
+            this.horizontalBlock(GCBlocks.CARGO_UNLOADER, model);
+
+            model = this.models().cubeAll("player_detector", tinDecor);
             this.simpleBlock(GCBlocks.PLAYER_DETECTOR, model);
-            model = this.models().cubeAll("hidden_redstone_wire", this.modLoc("block/tin_decoration"));
+            model = this.models().cubeAll("hidden_redstone_wire", tinDecor);
             this.simpleBlock(GCBlocks.HIDDEN_REDSTONE_WIRE, model);
-            model = this.models().cubeAll("hidden_redstone_repeater", this.modLoc("block/tin_decoration"));
+            model = this.models().cubeAll("hidden_redstone_repeater", tinDecor);
             this.simpleBlock(GCBlocks.HIDDEN_REDSTONE_REPEATER, model);
 
             model = this.models().cubeBottomTop("magnetic_crafting_table", this.modLoc("block/magnetic_crafting_table_side"), this.modLoc("block/magnetic_crafting_table_bottom"), this.modLoc("block/magnetic_crafting_table_top"));
             this.directionalBlock(GCBlocks.MAGNETIC_CRAFTING_TABLE, model);
 
-            model = this.models().withExistingParent("glowstone_torch", "block/template_torch").texture("torch", this.modLoc("block/glowstone_torch"));
-            this.simpleBlock(GCBlocks.GLOWSTONE_TORCH, model);
-            model = this.models().withExistingParent("unlit_torch", "block/template_torch").texture("torch", this.modLoc("block/unlit_torch"));
-            this.simpleBlock(GCBlocks.UNLIT_TORCH, model);
-            model = this.models().withExistingParent("lit_unlit_torch", "block/template_torch").texture("torch", this.modLoc("block/lit_unlit_torch"));
-            this.simpleBlock(GCBlocks.LIT_UNLIT_TORCH, model);
+            this.simpleBlock(GCBlocks.GLOWSTONE_TORCH, this.models().torch("glowstone_torch", this.modLoc("block/glowstone_torch")));
+            this.horizontalBlock(GCBlocks.WALL_GLOWSTONE_TORCH, this.models().torchWall("wall_glowstone_torch", this.modLoc("block/glowstone_torch")), 90);
 
-            model = this.models().withExistingParent("wall_glowstone_torch", "block/torch_wall").texture("torch", this.modLoc("block/glowstone_torch"));
-            this.horizontalBlock(GCBlocks.WALL_GLOWSTONE_TORCH, model, 90);
-            model = this.models().withExistingParent("wall_unlit_torch", "block/torch_wall").texture("torch", this.modLoc("block/unlit_torch"));
-            this.horizontalBlock(GCBlocks.WALL_UNLIT_TORCH, model, 90);
-            model = this.models().withExistingParent("wall_lit_unlit_torch", "block/torch_wall").texture("torch", this.modLoc("block/lit_unlit_torch"));
-            this.horizontalBlock(GCBlocks.WALL_LIT_UNLIT_TORCH, model, 90);
+            this.simpleBlock(GCBlocks.UNLIT_TORCH, this.models().torch("unlit_torch", this.modLoc("block/unlit_torch")));
+            this.horizontalBlock(GCBlocks.WALL_UNLIT_TORCH, this.models().torchWall("wall_unlit_torch", this.modLoc("block/unlit_torch")), 90);
+
+            this.simpleBlock(GCBlocks.LIT_UNLIT_TORCH, this.models().torch("lit_unlit_torch", this.modLoc("block/lit_unlit_torch")));
+            this.horizontalBlock(GCBlocks.WALL_LIT_UNLIT_TORCH, this.models().torchWall("wall_lit_unlit_torch", this.modLoc("block/lit_unlit_torch")), 90);
 
             model = this.models().cubeBottomTop("moon_turf", this.modLoc("block/moon_turf_side"), this.modLoc("block/moon_dirt"), this.modLoc("block/moon_turf_top"));
             this.simpleBlock(GCBlocks.MOON_TURF, model);
+
+            this.simpleFluid(GCFluids.OIL.getBlock());
+            this.simpleFluid(GCFluids.FUEL.getBlock());
+            this.simpleFluid(GCFluids.OXYGEN.getBlock());
+            this.simpleFluid(GCFluids.HYDROGEN.getBlock());
+        }
+
+        protected void simpleFluid(FlowingFluidBlock block)
+        {
+            this.getVariantBuilder(block).partialState().setModels(new ConfiguredModel(this.models().getBuilder(this.toString(block)).texture("particle", this.modLoc(block.getFluid().getAttributes().getStillTexture().getPath()))));
+        }
+
+        protected String toString(Block block)
+        {
+            return block.getRegistryName().getPath();
         }
     }
 
@@ -128,6 +196,7 @@ public class DataGeneratorGC
             this.parentedBlock(GCBlocks.METEORIC_IRON_BLOCK);
             this.parentedBlock(GCBlocks.MAGNETIC_CRAFTING_TABLE);
             this.parentedBlock(GCBlocks.TIER_1_TREASURE_CHEST, this.mcLoc("item/chest")).texture("particle", this.modLoc("block/tier_1_treasure_chest"));
+            this.parentedBlock(GCBlocks.PARACHEST, this.mcLoc("item/chest")).texture("particle", this.modLoc("block/parachest"));
             this.parentedBlock(GCBlocks.PLAYER_DETECTOR);
             this.parentedBlock(GCBlocks.HIDDEN_REDSTONE_WIRE);
             this.parentedBlock(GCBlocks.HIDDEN_REDSTONE_REPEATER);
@@ -138,7 +207,33 @@ public class DataGeneratorGC
             this.parentedBlock(GCBlocks.MOON_DIRT);
             this.parentedBlock(GCBlocks.MOON_DUNGEON_BRICKS);
             this.parentedBlock(GCBlocks.AIR_LOCK_FRAME);
+            this.parentedBlock(GCBlocks.TIN_DECORATION_BLOCK_1);
+            this.parentedBlock(GCBlocks.TIN_DECORATION_BLOCK_2);
+            this.parentedBlock(GCBlocks.OXYGEN_DETECTOR);
+            this.parentedBlock(GCBlocks.TELEMETRY_UNIT);
+            this.parentedBlock(GCBlocks.FALLEN_METEOR);
+            this.parentedBlock(GCBlocks.ROCKET_LAUNCH_PAD);
+            this.parentedBlock(GCBlocks.BUGGY_FUELING_PAD);
+            this.parentedBlock(GCBlocks.OXYGEN_BUBBLE_DISTRIBUTOR);
+            this.parentedBlock(GCBlocks.OXYGEN_COLLECTOR);
+            this.parentedBlock(GCBlocks.COMPRESSOR);
+            this.parentedBlock(GCBlocks.COAL_GENERATOR);
+            this.parentedBlock(GCBlocks.CHROMATIC_APPLICATOR);
+            this.parentedBlock(GCBlocks.BASIC_SOLAR_PANEL);
+            this.parentedBlock(GCBlocks.ADVANCED_SOLAR_PANEL);
+            this.parentedBlock(GCBlocks.OXYGEN_COMPRESSOR);
+            this.parentedBlock(GCBlocks.OXYGEN_DECOMPRESSOR);
+            this.parentedBlock(GCBlocks.OXYGEN_SEALER);
+            this.parentedBlock(GCBlocks.ELECTRIC_COMPRESSOR);
+            this.parentedBlock(GCBlocks.ELECTRIC_FURNACE);
+            this.parentedBlock(GCBlocks.REFINERY);
+            this.parentedBlock(GCBlocks.CARGO_LOADER);
+            this.parentedBlock(GCBlocks.CARGO_UNLOADER);
+            this.parentedBlock(GCBlocks.AIR_LOCK_CONTROLLER);
+            this.parentedBlock(GCBlocks.MOON_BOSS_SPAWNER, this.mcLoc("item/air"));
             this.itemGenerated(GCBlocks.GLOWSTONE_TORCH);
+            this.itemGenerated(GCBlocks.UNLIT_TORCH);
+            this.itemGenerated(GCBlocks.LIT_UNLIT_TORCH);
             this.itemGenerated(GCBlocks.ALUMINUM_WIRE.asItem());
             this.itemGenerated(GCBlocks.HEAVY_ALUMINUM_WIRE.asItem());
             this.itemGenerated(GCBlocks.SWITCHABLE_ALUMINUM_WIRE.asItem());
--- src/main/java/micdoodle8/mods/galacticraft/core/fluid/GCFluids.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/fluid/GCFluids.java
@@ -16,14 +16,13 @@ public class GCFluids
 {
     public static final GCFluidRegistry FLUIDS = new GCFluidRegistry();
 
-    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> OIL = registerLiquid("oil", fluidAttributes -> fluidAttributes.color(0xFF111111).density(800).viscosity(1500));
-    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> FUEL = registerLiquid("fuel", fluidAttributes -> fluidAttributes.color(0xFFDBDF16).density(400).viscosity(900));
-    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> OXYGEN = registerLiquid("oxygen", fluidAttributes -> fluidAttributes.color(0xFF6CE2FF).temperature(1).density(13).viscosity(295).gaseous());
-    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> HYDROGEN = registerLiquid("hydrogen", fluidAttributes -> fluidAttributes.color(0xFFFFFFFF).temperature(1).density(1).viscosity(295).gaseous());
+    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> OIL = registerLiquid("oil", "oil_still", "oil_flow", fluidAttributes -> fluidAttributes.color(0xFF111111).density(800).viscosity(1500));
+    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> FUEL = registerLiquid("fuel", "fuel_still", "fuel_flow", fluidAttributes -> fluidAttributes.color(0xFFDBDF16).density(400).viscosity(900));
+    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> OXYGEN = registerLiquid("oxygen", "oxygen_gas", "oxygen_gas", fluidAttributes -> fluidAttributes.color(0xFF6CE2FF).temperature(1).density(13).viscosity(295).gaseous());
+    public static final FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> HYDROGEN = registerLiquid("hydrogen", "hydrogen_gas", "hydrogen_gas", fluidAttributes -> fluidAttributes.color(0xFFFFFFFF).temperature(1).density(1).viscosity(295).gaseous());
 
-    private static FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> registerLiquid(String name, UnaryOperator<FluidAttributes.Builder> fluidAttributes)
+    private static FluidRegistrationEntry<Source, Flowing, FlowingFluidBlock, BucketItem> registerLiquid(String name, String stillTexture, String flowTexture, UnaryOperator<FluidAttributes.Builder> fluidAttributes)
     {
-        return FLUIDS.register(name, fluidAttributes.apply(FluidAttributes.builder(new ResourceLocation(Constants.MOD_ID_CORE, "block/liquid/liquid"),
-                new ResourceLocation(Constants.MOD_ID_CORE, "block/liquid/liquid_flow"))), Material.WATER);
+        return FLUIDS.register(name, fluidAttributes.apply(FluidAttributes.builder(new ResourceLocation(Constants.MOD_ID_CORE, "block/fluid/" + stillTexture), new ResourceLocation(Constants.MOD_ID_CORE, "block/fluid/" + flowTexture))), Material.WATER);
     }
 }
